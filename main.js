2cfe61fc9c5362172e678c89ac467041fba7ac5d
lucek666 <newcaliforniarep@gmail.com>
2017-07-12 20:46:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 var Discordie = require("discordie");
 var client = new Discordie();
 
-// Listen on port 8080
-server.listen(8080);
 
 // var game = {name: "FOnline 2"};
 // var streamingGame = {type: 1, name: "something", url: ""};
