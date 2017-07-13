9e9ffe4965339c5b404fbacb3c38d4b0382f45aa
lucek666 <newcaliforniarep@gmail.com>
2017-07-14 01:01:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var http = require('http');
 
 http.createServer(function(request, response){
-  respone.writeHead(200, {'Content-type':'text/plan'});
+  response.writeHead(200, {'Content-type':'text/plan'});
   response.write('Hello Node JS Server Response');
   response.end( );
 
