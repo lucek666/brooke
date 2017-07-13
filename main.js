73e7e9306020b248d89d39ea5f1840a896068aa5
lucek666 <newcaliforniarep@gmail.com>
2017-07-14 00:58:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 
+var http = require('http');
 
+http.createServer(function(request, response){
+  respone.writeHead(200, {'Content-type':'text/plan'});
+  response.write('Hello Node JS Server Response');
+  response.end( );
+
+}).listen(8080);
 
 
 
