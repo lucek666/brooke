f1a5dc223da7b0205b53350b18df1d6c00f78a51
lucek666 <newcaliforniarep@gmail.com>
2017-07-14 01:03:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 
-var http = require('http');
-
-http.createServer(function(request, response){
-  response.writeHead(200, {'Content-type':'text/plan'});
-  response.write('Hello Node JS Server Response');
-  response.end( );
-
-}).listen(8080);
-
-
 
 var Discordie = require("discordie");
 var client = new Discordie();
