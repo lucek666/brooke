0e7c735a6cc24385d35a7284ee10e7f620818d8f
lucek666 <newcaliforniarep@gmail.com>
2017-07-14 23:30:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 var Discordie = require("discordie");
 var client = new Discordie();
 
@@ -18,6 +17,7 @@ client.Dispatcher.on("GATEWAY_READY", e => {
 });
 
 client.Dispatcher.on("MESSAGE_CREATE", e => {
+
 
 
     if (e.message.content === "hello") {
@@ -92,27 +92,98 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
 } else if (e.message.content === "privet") {
         e.message.channel.sendMessage("Nice to see you " + e.message.author.nickMention + " ! Welcome to Broken Hills!");
     
+} else if (e.message.content === "Greetings") {
+        e.message.channel.sendMessage("Nice to see you " + e.message.author.nickMention + " ! Welcome to Broken Hills!");
+
+} else if (e.message.content === "greetings") {
+        e.message.channel.sendMessage("Nice to see you " + e.message.author.nickMention + " ! Welcome to Broken Hills!");
+
+
+     } else if (e.message.content === "Hi Marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+
+} else if (e.message.content === "Hi marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+  
+    } else if (e.message.content === "hi marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+
+} else if (e.message.content === "hi Marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+
     
+ } else if (e.message.content === "Hello Marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+
+} else if (e.message.content === "Hello marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+  
+    } else if (e.message.content === "hello marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+
+} else if (e.message.content === "hello Marcus") {
+        e.message.channel.sendMessage("Whats up " + e.message.author.nickMention + " !");
+
+
+
+
 } else if (e.message.content === "cheers") {
         e.message.channel.sendMessage("<:TVMuUrt:333263734938140672>");
 
   } else if (e.message.content === "Cheers") {
         e.message.channel.sendMessage("<:TVMuUrt:333263734938140672>");  
       
-      
-      
+      } else if (e.message.content === "shit") {
+        e.message.channel.sendMessage("<:cNMC4h3:333263734623698956>");
+
+  } else if (e.message.content === "Shit") {
+        e.message.channel.sendMessage("<:cNMC4h3:333263734623698956>");
+
+      } else if (e.message.content === "fuck") {
+        e.message.channel.sendMessage("<:azHInbg:333263707620769816>");
+
+  } else if (e.message.content === "Fuck") {
+        e.message.channel.sendMessage("<:azHInbg:333263707620769816>");
+
+} else if (e.message.content === "Hehe") {
+        e.message.channel.sendMessage("<:7BQx50y:333263658983358479>");
+
+  } else if (e.message.content === "Haha") {
+        e.message.channel.sendMessage("<:7BQx50y:333263658983358479>");
+
+} else if (e.message.content === "He he") {
+        e.message.channel.sendMessage("<:7BQx50y:333263658983358479>");
+
+  } else if (e.message.content === "Ha ha") {
+        e.message.channel.sendMessage("<:7BQx50y:333263658983358479>");
+
+
+
+
+
 
  //   } else if (e.message.content === "Thant?") {
+        e.message.channel.sendMessage("I bet he is in sierra caves. Thant is real caveman.");
+// } else if (e.message.content === "thant?") {
         e.message.channel.sendMessage("I bet he is in sierra caves. Thant is real caveman.");
 
 // } else if (e.message.content === "Vick?") {
         e.message.channel.sendMessage("I saw him last night in bar. I can't describe how drunk he was.");
+// } else if (e.message.content === "vick?") {
+        e.message.channel.sendMessage("I saw him last night in bar. I can't describe how drunk he was.");
 
+// } else if (e.message.content === "Marcus?") {
+        e.message.channel.sendMessage("Don't bother me, I'm too busy at idling in downtown.");
+// } else if (e.message.content === "marcus?") {
+        e.message.channel.sendMessage("Don't bother me, I'm too busy at idling in downtown.");
  
-    
+    // } else if (e.message.content === "Lucek?") {
+        e.message.channel.sendMessage("");
+// } else if (e.message.content === "lucek?") {
+        e.message.channel.sendMessage("Don't bother me, I'm too busy at idling in downtown.");
+
+
     }
-
-
 
 if (e.message.content === "tips"){
         var eightball = ["Improved Flamethrower is your best friend when comes to dealing with big amount of low HP enemies. Oh it's also perfect for your local Barbeque.","Going to visit Toxic Caves? You better find some Rubber Boots first!","Going to Glow? Withouth Rad-X you will melt in seconds. Always have anti-radiation drugs when going there. Oh also a gun will be handy.", "In addition to aiming at your enemies, some weapons can also aim at hexes. For example Rocket Launcher can blow doors! To enter hex aiming mode press CTRL while having attack coursor.","Necropolis? More like PKpolis. Warehouse? Nah it's actually Slaughterhouse. Never venture into dungeons withouth friends as enemy players will lurk there.","You enjoy special encounters? Consider walking. You will never hit special encounter whileusing a car.",
@@ -127,3 +198,4 @@ if (e.message.content === "tips"){
 
 
 });
+
